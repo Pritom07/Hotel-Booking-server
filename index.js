6cdee472b287a3d29bd16dcb737b1988adc54c05
Pritom07 <pritommohajon4545@gmail.com>
2025-09-12 20:06:42 +0600

    Database connection complete and Get Data successfully

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -7,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Password}@cluster0.r5e76.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.r5e76.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -19,10 +20,16 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    const roomCollection = client.db("Hotel_Booking_System").collection("Room");
-    const bookingCollection = client
-      .db("Hotel_Booking_System")
-      .collection("Booking");
+    const roomsCollection = client.db("HotelBookingSystem").collection("rooms");
+    const bookingsCollection = client
+      .db("HotelBookingSystem")
+      .collection("bookings");
+
+    app.get("/rooms", async (req, res) => {
+      const cursor = roomsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
